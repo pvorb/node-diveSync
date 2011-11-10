ffebf3b5a4d001d24d5982f2be4a8185820e03bb
Dave Geddes <davidcgeddes@gmail.com>
2011-11-10 16:25:08 -0700

    added the option to ignore directories
    you can now specify an array of directories you'd like to skip over,
    relative to the process.cwd().
    Example usage:
    
    diveSync(process.cwd(), {ignore: ['node_modules',
    'backend/compressed']}, function(err, file){
      if(err) throw err;
    	if(file.split('.').pop() === 'less'){
    		//compile less to css
    	}
    });

--- diveSync.js
+++ diveSync.js
@@ -1,51 +1,62 @@
-var fs = require('fs'),
-    append = require('append');
+var fs = require('fs'), append = require('append'), ignoreDirs = [], ignoreDirsCreated = false;
 
 // General function
 var diveSync = function(dir, opt, action) {
 
-  // action is the last argument
-  action = arguments[arguments.length - 1];
+	// action is the last argument
+	action = arguments[arguments.length - 1];
 
-  // default options
-  var defaultOpt = {
-    recursive: true,
-    all: false,
-    directories: false
-  };
+	// default options
+	var defaultOpt = {
+		recursive:true,
+		all:false,
+		directories:false
+	};
 
-  // ensure opt is an object
-  if (typeof opt != 'object')
-    opt = {};
+	// ensure opt is an object
+	if (typeof opt != 'object')
+		opt = {};
 
-  opt = append(defaultOpt, opt);
+	opt = append(defaultOpt, opt);
 
-  // Read the directory
-  var list = fs.readdirSync(dir);
+	//if opt.ignore create the ignoring array
+	if (!ignoreDirsCreated && opt.ignore) {
+		opt.ignore.forEach(function(item) {
+			ignoreDirs.push(process.cwd() + '/' + item);
+		});
+		ignoreDirsCreated = true;
+	}
 
-  // For every file in the list
-  list.forEach(function(file) {
-    if (opt.all || file[0] != '.') {
-      // Full path of that file
-      var path = dir + '/' + file;
-      // Get the file's stats
-      var stat = fs.statSync(path);
+	// Read the directory unless ignoring it
+	if (ignoreDirs.indexOf(dir) != -1) {
+		return;
+	}
 
-      // If the file is a directory
-      if (stat && stat.isDirectory()) {
-        // Call the action if enabled for directories
-        if (opt.directories)
-          action(null, path);
+	var list = fs.readdirSync(dir);
 
-        // Dive into the directory
-        if (opt.recursive)
-          diveSync(path, opt, action);
-      } else {
-        // Call the action
-        action(null, path);
-      }
-    }
-  });
+	// For every file in the list
+	list.forEach(function(file) {
+		if (opt.all || file[0] != '.') {
+			// Full path of that file
+			var path = dir + '/' + file;
+			// Get the file's stats
+			var stat = fs.statSync(path);
+
+			// If the file is a directory
+			if (stat && stat.isDirectory()) {
+				// Call the action if enabled for directories
+				if (opt.directories)
+					action(null, path);
+
+				// Dive into the directory
+				if (opt.recursive)
+					diveSync(path, opt, action);
+			} else {
+				// Call the action
+				action(null, path);
+			}
+		}
+	});
 };
 
 module.exports = diveSync;
